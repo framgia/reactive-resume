5be3c2b5c9cfe30e2c1aaa7f4549cd43782714b1
tungpt-3613 <pham.thanh.tung@sun-asterisk.com>
2026-03-12 13:46:27 +0700

    chore: remove unused PWA icon files and comment out related configurations in vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -66,22 +66,22 @@ const config = defineConfig({
 						sizes: "128x128",
 						type: "image/x-icon",
 					},
-					{
-						src: "pwa-64x64.png",
-						sizes: "64x64",
-						type: "image/png",
-					},
-					{
-						src: "pwa-192x192.png",
-						sizes: "192x192",
-						type: "image/png",
-					},
-					{
-						src: "pwa-512x512.png",
-						sizes: "512x512",
-						type: "image/png",
-						purpose: "any",
-					},
+					// {
+					// 	src: "pwa-64x64.png",
+					// 	sizes: "64x64",
+					// 	type: "image/png",
+					// },
+					// {
+					// 	src: "pwa-192x192.png",
+					// 	sizes: "192x192",
+					// 	type: "image/png",
+					// },
+					// {
+					// 	src: "pwa-512x512.png",
+					// 	sizes: "512x512",
+					// 	type: "image/png",
+					// 	purpose: "any",
+					// },
 					{
 						src: "maskable-icon-512x512.png",
 						sizes: "512x512",
