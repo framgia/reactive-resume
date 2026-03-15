ec8494ab429db5ab4acefd8447799872ecc986ef
Tim Pham <phtung.99@gmail.com>
2026-03-15 13:38:39 +0700

    refactor: update slider component to use new data attributes for orientation handling

--- src/components/ui/slider.tsx
+++ src/components/ui/slider.tsx
@@ -23,18 +23,18 @@ function Slider({
 			min={min}
 			max={max}
 			className={cn(
-				"relative flex w-full touch-none select-none items-center data-vertical:h-full data-vertical:min-h-40 data-vertical:w-auto data-vertical:flex-col data-disabled:opacity-50",
+				"relative flex w-full touch-none select-none items-center data-[orientation=vertical]:h-full data-[orientation=vertical]:min-h-40 data-[orientation=vertical]:w-auto data-[orientation=vertical]:flex-col data-disabled:opacity-50",
 				className,
 			)}
 			{...props}
 		>
 			<SliderPrimitive.Track
 				data-slot="slider-track"
-				className="relative grow overflow-hidden rounded-full bg-muted data-horizontal:h-1.5 data-vertical:h-full data-horizontal:w-full data-vertical:w-1.5"
+				className="relative grow overflow-hidden rounded-full bg-muted data-[orientation=horizontal]:h-1.5 data-[orientation=vertical]:h-full data-[orientation=horizontal]:w-full data-[orientation=vertical]:w-1.5"
 			>
 				<SliderPrimitive.Range
 					data-slot="slider-range"
-					className="absolute select-none bg-primary data-horizontal:h-full data-vertical:w-full"
+					className="absolute select-none bg-primary data-[orientation=horizontal]:h-full data-[orientation=vertical]:w-full"
 				/>
 			</SliderPrimitive.Track>
 			{Array.from({ length: _values.length }, (_, index) => (
